211a8f4644ec5e1809801f6c164ab4acf4a4dc93
Gavin Davies <gavin.davies1@digital.hmrc.gov.uk>
2024-05-14 13:16:10 +0100

    x

--- src/emit_heartbeat_metric.py
+++ src/emit_heartbeat_metric.py
@@ -3,6 +3,7 @@ import os
 
 import graphyte
 
+
 def lambda_handler(event):
     graphite_host = os.getenv("GRAPHITE_HOST", "graphite-collectd")
     graphite_metric_prefix = os.getenv("GRAPHITE_METRIC_PREFIX", "container-insights")
